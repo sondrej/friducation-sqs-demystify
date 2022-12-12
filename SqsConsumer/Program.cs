578115a0584a83accffc50e13a1803b11087502d
sondrej <sondre.jensen@visma.com>
2022-12-12 16:49:02 +0100

    Add some comments and move try-catch to surround usage of sqsclient

--- SqsConsumer/Program.cs
+++ SqsConsumer/Program.cs
@@ -17,36 +17,47 @@ Console.WriteLine($"{handlerName}::Subscribed to SQS Topic");
 
 while (true)
 {
-    var msg = await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+    try
     {
-        QueueUrl = queueUrl,
-        MaxNumberOfMessages = 1,
-        WaitTimeSeconds = 60
-    });
+        var msg = await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+        {
+            QueueUrl = queueUrl,
+            MaxNumberOfMessages = 1,
+            WaitTimeSeconds = 60
+        });
 
-    foreach (var message in msg.Messages)
-    {
-        try
+        // Any message we receive here is "invisible" on the queue with a timeout
+        // If we fail to process the message (IE remove the message from the queue) within the timeout, 
+        // it will be made receivable for other consumers.
+        foreach (var message in msg.Messages)
         {
             await handler(message);
         }
-        catch (HandleMessageException ex)
-        {
-            Console.Error.WriteLine($"{handlerName}::{ex.Message}");
-        }
+    }
+    catch (HandleMessageException ex)
+    {
+        // No need to crash the consumer process here, but assume any other exception is one we cannot recover from.
+        // In a fargate instance, we'd let AWS restart the instance when we get any other exception.
+        // In a lambda we would just let this error propagate as well, and always delete a message when we're done with it.
+        Console.Error.WriteLine($"{handlerName}::{ex.Message}");
     }
 }
 
-static Func<Message, Task> CreateHandleMessage(IAmazonSQS sqsClient, string queueArn, string handlerName) => async message =>
+static Func<Message, Task> CreateHandleMessage(IAmazonSQS sqsClient, string queueArn, string handlerName)
 {
-    Console.WriteLine($"{handlerName}::Received message: {message.Body}");
-    if (message.Body.Contains("fail"))
-        throw new HandleMessageException(message);
-    await sqsClient.DeleteMessageAsync(queueArn, message.ReceiptHandle, CancellationToken.None);
-};
+    return async message =>
+    {
+        Console.WriteLine($"{handlerName}::Received message: {message.Body}");
+        if (message.Body.Contains("fail"))
+            throw new HandleMessageException(message);
+        // We're done processing this message, it can now be removed from the queue
+        await sqsClient.DeleteMessageAsync(queueArn, message.ReceiptHandle, CancellationToken.None);
+    };
+}
 
 internal sealed class HandleMessageException : Exception
 {
+    // Could be useful to include some properties from the message in the logs, hence this property
     public Message SqsMessage { get; }
 
     public HandleMessageException(Message message) : base($"Could not handle message {message.MessageId}")
